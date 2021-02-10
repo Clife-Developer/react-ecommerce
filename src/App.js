469a3a7ac34ae0d4dbf3c55cc3ee968ddf373e08
Clife-Developer <clife.united@gmail.com>
2021-02-11 00:14:22 +0200

    modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import Products from './componets/products/products'
+import React,{useState,useEffect} from 'react'
+import {Products,Navbar} from './componets'
+import {commerce} from './lib/commerce'
+
 function App() {
+    const [products,setProducts]=useState([]);
+
+    const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+    useEffect(()=>{
+        fetchProducts();
+    },[])
+
+    console.log(products)
     return (
         <div>
+            <Navbar/>
            <Products/>
         </div>
     )
--- src/componets/products/products.js
+++ src/componets/products/products.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './product/product'
-
+import useStyles from './styles'
 const products=[
-    {id:1,name:"Shoes",description:"Running Shoes",price:'$10'},
-    {id:2,name:"Macbook",description:"Apple Macbook",price:"$50"}
+    {id:1,name:"Shoes",description:"Running Shoes",
+    price:'$10',image:"https://tse1.mm.bing.net/th?id=OIP.ICR9uv_p2pxsuV-xRo8k1AHaHa&pid=Api&P=0&w=300&h=300"},
+    {id:2,name:"Macbook",description:"Apple Macbook",
+    price:"$50",image:"https://images.techhive.com/images/article/2014/12/macbook_air-100538062-large.jpg"}
 ]
 const Products=()=>{
+    const classes=useStyles();
     return(
-        <main>
-            <Grid container justify="center" spacing={4}>
-              {products.map(product=>{
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
+             <Grid container justify="center" spacing={4}>
+               {products.map(product=>{
                  return(
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                       <Product product={product}/>
-                </Grid> 
-            )})}
+                     <Product product={product}/>
+               </Grid> 
+             )})}
         </Grid>
     </main>
    )
