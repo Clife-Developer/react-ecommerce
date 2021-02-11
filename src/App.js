cb3f3ca28c5f78304c5a1d050eb0f25dc2e622cf
Clife-Developer <clife.united@gmail.com>
2021-02-12 00:00:22 +0200

    components modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React,{useState,useEffect} from 'react'
-import {Products,Navbar} from './componets'
+import {Products,Navbar, Cart} from './componets'
 import {commerce} from './lib/commerce'
 
 function App() {
     const [products,setProducts]=useState([]);
+    const [cart,setCart]=useState({});
 
     const fetchProducts = async () => {
     const { data } = await commerce.products.list();
-
     setProducts(data);
   };
 
+    const fetchCart= async ()=>{
+        const cart=await commerce.cart.retrieve();
+        setCart(cart)
+    }
+
+    const handleAddToCart= async (productId,quantity)=>{
+        const item =await commerce.cart.add(productId,quantity)
+        setCart(item.cart)
+    }
+
     useEffect(()=>{
         fetchProducts();
+        fetchCart()
     },[])
 
-    console.log(products)
     return (
         <div>
-            <Navbar/>
-           <Products/>
+            <Navbar totalItems={cart.total_items}/>
+            {/*<Products products={products} onAddToCart={handleAddToCart}/>*/}
+             <Cart cart={cart}/>
         </div>
     )
 }
--- src/componets/products/products.js
+++ src/componets/products/products.js
@@ -2,13 +2,8 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './product/product'
 import useStyles from './styles'
-const products=[
-    {id:1,name:"Shoes",description:"Running Shoes",
-    price:'$10',image:"https://tse1.mm.bing.net/th?id=OIP.ICR9uv_p2pxsuV-xRo8k1AHaHa&pid=Api&P=0&w=300&h=300"},
-    {id:2,name:"Macbook",description:"Apple Macbook",
-    price:"$50",image:"https://images.techhive.com/images/article/2014/12/macbook_air-100538062-large.jpg"}
-]
-const Products=()=>{
+
+const Products=({products,onAddToCart})=>{
     const classes=useStyles();
     return(
         <main className={classes.content}>
@@ -17,7 +12,7 @@ const Products=()=>{
                {products.map(product=>{
                  return(
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                     <Product product={product}/>
+                     <Product product={product} onAddToCart={onAddToCart}/>
                </Grid> 
              )})}
         </Grid>
