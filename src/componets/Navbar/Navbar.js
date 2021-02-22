8a1763bea560bddf80d90176d2d7a4bb837e78a7
Clife-Developer <clife.united@gmail.com>
2021-02-22 02:07:02 +0200

    Navbar modified

--- src/componets/Navbar/Navbar.js
+++ src/componets/Navbar/Navbar.js
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
-//import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 
-import Logo from '../../assets/logo.jpg';
+import Logo from '../../assets/logo2.png';
 import useStyles from './styles';
 
 const Navbar = ({totalItems}) => {
     const classes=useStyles();
+    const location=useLocation();
+   
     return (
         <>
         <AppBar position="fixed" className={classes.appBar} color="inherit">
-        <Toolbar>
-          <Typography to="/" variant="h6" className={classes.title} color="inherit">
-            <img src={Logo} alt="commerce.js" height="25px" className={classes.image} />CLIFE'S COMMERCE
+           <Toolbar>
+             <Typography to="/" component={Link} variant="h5" className={classes.title} color="inherit">
+               <img src={Logo} alt="commerce.js" height="70px" className={classes.image} />
           </Typography>
           <div className={classes.grow} />
-
-          <div className={classes.button}>
-            <IconButton to="/cart" aria-label="Show cart items" color="inherit">
-              <Badge badgeContent={totalItems} color="secondary">
-                <ShoppingCart />
-              </Badge>
-            </IconButton>
-          </div>
-        </Toolbar>
+             {location.pathname=='/' && (
+               <div className={classes.button}>
+                 <IconButton to="/cart" component={Link} aria-label="Show cart items" color="inherit">
+                 <Badge badgeContent={totalItems} color="secondary">
+                   <ShoppingCart />
+                 </Badge>
+               </IconButton>
+            </div>)}
+         </Toolbar>
       </AppBar>
-        </>
-    )
+    </>
+  )
 }
 
 export default Navbar
